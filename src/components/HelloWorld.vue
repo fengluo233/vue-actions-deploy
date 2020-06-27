d7c9a73d7cf29100617b39efdbccca5d190df7c8
LIMnH996 <45155695+LIMnH996@users.noreply.github.com>
2020-06-27 23:37:19 +0800

    Update HelloWorld.vue

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,7 +2,7 @@
   <div class="hello">
     <h1>{{ msg }}</h1>
     <p>
-      我不太好
+      我不太好。////
       你好
       For a guide and recipes daadadadadadadadadadadadadadadadadad on how to configure / customize this project,<br>
       check out the
